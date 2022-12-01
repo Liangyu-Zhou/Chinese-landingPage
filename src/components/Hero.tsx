dda2f899bc1cc0b0e9aac1a9c3f7c81e2b70b718
Liangyu-Zhou <17816872833@163.com>
2022-12-01 08:26:14 +0000

    zly init commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import Typed from 'react-typed';
 const Hero = () => {
   return (
-    <div className='text-white'>
+    <div className='text-white bg-[#222222]'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+            <p className='text-[#F26C4F] font-bold p-2'>TOKENIZED CONTENT SHARING PLATFORM</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Chinese.org</h1>
             <div className='flex justify-center items-center'>
-                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Earn tokens by sharing</p>
                 <Typed 
                 className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-                strings={['BTB', 'BTC', 'SASS']} 
+                strings={['posts', 'ideas', 'likes']} 
                 typeSpeed={120}
                 backSpeed={140} 
                 loop
                  />
             </div>
-            <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC and SASS platforms.</p>
-            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+            <p className='md:text-2xl text-xl font-bold text-gray-500'>Share your creations to increase revenue for ETH, MATIC and other token.</p>
+            <button className='bg-[#F26C4F] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Login with Metamask</button>
         </div>
     </div>  
   )
